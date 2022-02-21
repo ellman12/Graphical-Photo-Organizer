7a2b02e48421b94d0fdc0d978917d5c30def8a23
Elliott <ellduc4@gmail.com>
2022-02-21 16:07:45 -0600

    finish basic gui layout i think

--- Graphical-Photo-Organizer/GraphicalPhotoOrganizer.Designer.cs
+++ Graphical-Photo-Organizer/GraphicalPhotoOrganizer.Designer.cs
@@ -28,32 +28,32 @@ partial class GraphicalPhotoOrganizer
     /// </summary>
     private void InitializeComponent()
     {
-            this.pictureBox = new System.Windows.Forms.PictureBox();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.datePickerLabel = new System.Windows.Forms.Label();
             this.filenameLabel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.destPathTextLabel = new System.Windows.Forms.Label();
-            this.destPathLabel = new System.Windows.Forms.Label();
             this.chooseSrcDirBtn = new System.Windows.Forms.Button();
-            this.srcDirTextLabel = new System.Windows.Forms.Label();
             this.srcDirLabel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.setupGroupBox = new System.Windows.Forms.GroupBox();
+            this.chooseDestDirBtn = new System.Windows.Forms.Button();
+            this.destDirLabel = new System.Windows.Forms.Label();
+            this.destPathLabel = new System.Windows.Forms.Label();
+            this.currentItemGroupBox = new System.Windows.Forms.GroupBox();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.skipPhotoBtn = new System.Windows.Forms.Button();
+            this.deletePhotoBtn = new System.Windows.Forms.Button();
+            this.nextPhotoBtn = new System.Windows.Forms.Button();
+            this.applyBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.setupGroupBox.SuspendLayout();
+            this.currentItemGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox
-            // 
-            this.pictureBox.ImageLocation = "C:/Users/Elliott/Pictures/Steam Screenshots/413150_20220212202151_1.png";
-            this.pictureBox.Location = new System.Drawing.Point(497, 22);
-            this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(518, 292);
-            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox.TabIndex = 0;
-            this.pictureBox.TabStop = false;
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(12, 131);
+            this.monthCalendar1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monthCalendar1.Location = new System.Drawing.Point(6, 123);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 1;
             // 
@@ -61,7 +61,7 @@ partial class GraphicalPhotoOrganizer
             // 
             this.datePickerLabel.AutoSize = true;
             this.datePickerLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.datePickerLabel.Location = new System.Drawing.Point(12, 94);
+            this.datePickerLabel.Location = new System.Drawing.Point(6, 94);
             this.datePickerLabel.Name = "datePickerLabel";
             this.datePickerLabel.Size = new System.Drawing.Size(107, 28);
             this.datePickerLabel.TabIndex = 2;
@@ -71,7 +71,7 @@ partial class GraphicalPhotoOrganizer
             // 
             this.filenameLabel.AutoSize = true;
             this.filenameLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.filenameLabel.Location = new System.Drawing.Point(12, 9);
+            this.filenameLabel.Location = new System.Drawing.Point(6, 26);
             this.filenameLabel.Name = "filenameLabel";
             this.filenameLabel.Size = new System.Drawing.Size(90, 28);
             this.filenameLabel.TabIndex = 3;
@@ -79,91 +79,196 @@ partial class GraphicalPhotoOrganizer
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(6, 57);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(312, 31);
+            this.textBox1.Size = new System.Drawing.Size(312, 34);
             this.textBox1.TabIndex = 4;
-            // 
-            // destPathTextLabel
-            // 
-            this.destPathTextLabel.AutoSize = true;
-            this.destPathTextLabel.Location = new System.Drawing.Point(567, 496);
-            this.destPathTextLabel.Name = "destPathTextLabel";
-            this.destPathTextLabel.Size = new System.Drawing.Size(150, 25);
-            this.destPathTextLabel.TabIndex = 5;
-            this.destPathTextLabel.Text = "Destination Path: ";
-            // 
-            // destPathLabel
-            // 
-            this.destPathLabel.AutoSize = true;
-            this.destPathLabel.Location = new System.Drawing.Point(712, 496);
-            this.destPathLabel.Name = "destPathLabel";
-            this.destPathLabel.Size = new System.Drawing.Size(80, 25);
-            this.destPathLabel.TabIndex = 6;
-            this.destPathLabel.Text = "destPath";
             // 
             // chooseSrcDirBtn
             // 
-            this.chooseSrcDirBtn.Location = new System.Drawing.Point(12, 522);
+            this.chooseSrcDirBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chooseSrcDirBtn.Location = new System.Drawing.Point(6, 36);
             this.chooseSrcDirBtn.Name = "chooseSrcDirBtn";
-            this.chooseSrcDirBtn.Size = new System.Drawing.Size(199, 34);
+            this.chooseSrcDirBtn.Size = new System.Drawing.Size(312, 41);
             this.chooseSrcDirBtn.TabIndex = 7;
             this.chooseSrcDirBtn.Text = "Choose Source Folder";
             this.chooseSrcDirBtn.UseVisualStyleBackColor = true;
             this.chooseSrcDirBtn.Click += new System.EventHandler(this.chooseSrcDirBtn_Click);
             // 
-            // srcDirTextLabel
-            // 
-            this.srcDirTextLabel.AutoSize = true;
-            this.srcDirTextLabel.Location = new System.Drawing.Point(14, 586);
-            this.srcDirTextLabel.Name = "srcDirTextLabel";
-            this.srcDirTextLabel.Size = new System.Drawing.Size(98, 25);
-            this.srcDirTextLabel.TabIndex = 8;
-            this.srcDirTextLabel.Text = "Source Dir:";
-            // 
             // srcDirLabel
             // 
             this.srcDirLabel.AutoSize = true;
-            this.srcDirLabel.Location = new System.Drawing.Point(113, 586);
+            this.srcDirLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.srcDirLabel.Location = new System.Drawing.Point(6, 80);
             this.srcDirLabel.Name = "srcDirLabel";
-            this.srcDirLabel.Size = new System.Drawing.Size(33, 25);
+            this.srcDirLabel.Size = new System.Drawing.Size(36, 28);
             this.srcDirLabel.TabIndex = 9;
             this.srcDirLabel.Text = "dir";
+            // 
+            // setupGroupBox
+            // 
+            this.setupGroupBox.Controls.Add(this.chooseDestDirBtn);
+            this.setupGroupBox.Controls.Add(this.destDirLabel);
+            this.setupGroupBox.Controls.Add(this.chooseSrcDirBtn);
+            this.setupGroupBox.Controls.Add(this.srcDirLabel);
+            this.setupGroupBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.setupGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.setupGroupBox.Name = "setupGroupBox";
+            this.setupGroupBox.Size = new System.Drawing.Size(326, 186);
+            this.setupGroupBox.TabIndex = 10;
+            this.setupGroupBox.TabStop = false;
+            this.setupGroupBox.Text = "Setup";
+            // 
+            // chooseDestDirBtn
+            // 
+            this.chooseDestDirBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chooseDestDirBtn.Location = new System.Drawing.Point(6, 114);
+            this.chooseDestDirBtn.Name = "chooseDestDirBtn";
+            this.chooseDestDirBtn.Size = new System.Drawing.Size(311, 41);
+            this.chooseDestDirBtn.TabIndex = 10;
+            this.chooseDestDirBtn.Text = "Choose Destination Root Folder";
+            this.chooseDestDirBtn.UseVisualStyleBackColor = true;
+            this.chooseDestDirBtn.Click += new System.EventHandler(this.chooseDestDirBtn_Click);
+            // 
+            // destDirLabel
+            // 
+            this.destDirLabel.AutoSize = true;
+            this.destDirLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.destDirLabel.Location = new System.Drawing.Point(6, 155);
+            this.destDirLabel.Name = "destDirLabel";
+            this.destDirLabel.Size = new System.Drawing.Size(36, 28);
+            this.destDirLabel.TabIndex = 12;
+            this.destDirLabel.Text = "dir";
+            // 
+            // destPathLabel
+            // 
+            this.destPathLabel.AutoSize = true;
+            this.destPathLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.destPathLabel.Location = new System.Drawing.Point(6, 519);
+            this.destPathLabel.Name = "destPathLabel";
+            this.destPathLabel.Size = new System.Drawing.Size(80, 25);
+            this.destPathLabel.TabIndex = 6;
+            this.destPathLabel.Text = "destPath";
+            // 
+            // currentItemGroupBox
+            // 
+            this.currentItemGroupBox.Controls.Add(this.resetBtn);
+            this.currentItemGroupBox.Controls.Add(this.skipPhotoBtn);
+            this.currentItemGroupBox.Controls.Add(this.deletePhotoBtn);
+            this.currentItemGroupBox.Controls.Add(this.destPathLabel);
+            this.currentItemGroupBox.Controls.Add(this.nextPhotoBtn);
+            this.currentItemGroupBox.Controls.Add(this.applyBtn);
+            this.currentItemGroupBox.Controls.Add(this.filenameLabel);
+            this.currentItemGroupBox.Controls.Add(this.textBox1);
+            this.currentItemGroupBox.Controls.Add(this.monthCalendar1);
+            this.currentItemGroupBox.Controls.Add(this.datePickerLabel);
+            this.currentItemGroupBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.currentItemGroupBox.Location = new System.Drawing.Point(12, 204);
+            this.currentItemGroupBox.Name = "currentItemGroupBox";
+            this.currentItemGroupBox.Size = new System.Drawing.Size(326, 553);
+            this.currentItemGroupBox.TabIndex = 11;
+            this.currentItemGroupBox.TabStop = false;
+            this.currentItemGroupBox.Text = "Current Photo";
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resetBtn.Location = new System.Drawing.Point(6, 476);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(154, 41);
+            this.resetBtn.TabIndex = 17;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            // 
+            // skipPhotoBtn
+            // 
+            this.skipPhotoBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.skipPhotoBtn.Location = new System.Drawing.Point(164, 429);
+            this.skipPhotoBtn.Name = "skipPhotoBtn";
+            this.skipPhotoBtn.Size = new System.Drawing.Size(154, 41);
+            this.skipPhotoBtn.TabIndex = 16;
+            this.skipPhotoBtn.Text = "Skip Photo";
+            this.skipPhotoBtn.UseVisualStyleBackColor = true;
+            // 
+            // deletePhotoBtn
+            // 
+            this.deletePhotoBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.deletePhotoBtn.Location = new System.Drawing.Point(6, 429);
+            this.deletePhotoBtn.Name = "deletePhotoBtn";
+            this.deletePhotoBtn.Size = new System.Drawing.Size(154, 41);
+            this.deletePhotoBtn.TabIndex = 15;
+            this.deletePhotoBtn.Text = "Delete Photo";
+            this.deletePhotoBtn.UseVisualStyleBackColor = true;
+            // 
+            // nextPhotoBtn
+            // 
+            this.nextPhotoBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nextPhotoBtn.Location = new System.Drawing.Point(164, 382);
+            this.nextPhotoBtn.Name = "nextPhotoBtn";
+            this.nextPhotoBtn.Size = new System.Drawing.Size(154, 41);
+            this.nextPhotoBtn.TabIndex = 14;
+            this.nextPhotoBtn.Text = "Next Photo";
+            this.nextPhotoBtn.UseVisualStyleBackColor = true;
+            // 
+            // applyBtn
+            // 
+            this.applyBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.applyBtn.Location = new System.Drawing.Point(6, 382);
+            this.applyBtn.Name = "applyBtn";
+            this.applyBtn.Size = new System.Drawing.Size(154, 41);
+            this.applyBtn.TabIndex = 13;
+            this.applyBtn.Text = "Apply";
+            this.applyBtn.UseVisualStyleBackColor = true;
+            this.applyBtn.Click += new System.EventHandler(this.applyBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(357, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1295, 730);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
             // 
             // GraphicalPhotoOrganizer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1206, 756);
-            this.Controls.Add(this.srcDirLabel);
-            this.Controls.Add(this.srcDirTextLabel);
-            this.Controls.Add(this.chooseSrcDirBtn);
-            this.Controls.Add(this.destPathLabel);
-            this.Controls.Add(this.destPathTextLabel);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.filenameLabel);
-            this.Controls.Add(this.datePickerLabel);
-            this.Controls.Add(this.monthCalendar1);
-            this.Controls.Add(this.pictureBox);
+            this.ClientSize = new System.Drawing.Size(1650, 767);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.currentItemGroupBox);
+            this.Controls.Add(this.setupGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "GraphicalPhotoOrganizer";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Graphical Photo Organizer";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.setupGroupBox.ResumeLayout(false);
+            this.setupGroupBox.PerformLayout();
+            this.currentItemGroupBox.ResumeLayout(false);
+            this.currentItemGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
     }
 
     #endregion
-
-    private PictureBox pictureBox;
     private MonthCalendar monthCalendar1;
     private Label datePickerLabel;
     private Label filenameLabel;
     private TextBox textBox1;
-    private Label destPathTextLabel;
-    private Label destPathLabel;
     private Button chooseSrcDirBtn;
-    private Label srcDirTextLabel;
     private Label srcDirLabel;
+    private GroupBox setupGroupBox;
+    private Button chooseDestDirBtn;
+    private Label destDirLabel;
+    private GroupBox currentItemGroupBox;
+    private PictureBox pictureBox1;
+    private Button nextPhotoBtn;
+    private Button applyBtn;
+    private Button skipPhotoBtn;
+    private Button deletePhotoBtn;
+    private Label destPathLabel;
+    private Button resetBtn;
 }
--- Graphical-Photo-Organizer/GraphicalPhotoOrganizer.cs
+++ Graphical-Photo-Organizer/GraphicalPhotoOrganizer.cs
@@ -3,6 +3,7 @@ namespace Graphical_Photo_Organizer;
 public partial class GraphicalPhotoOrganizer : Form
 {
     private List<string> unsortedFiles = new();
+    private string sortedDirRootPath = "";
     
     public GraphicalPhotoOrganizer()
     {
@@ -16,10 +17,36 @@ public partial class GraphicalPhotoOrganizer : Form
         fbd.Description = "Select Folder of Images to Sort";
         DialogResult result = fbd.ShowDialog();
 
-        if (result == DialogResult.OK && !String.IsNullOrWhiteSpace(fbd.SelectedPath))
+        if (result == DialogResult.OK && !String.IsNullOrWhiteSpace(fbd.SelectedPath) && Directory.Exists(fbd.SelectedPath))
         {
             unsortedFiles = Directory.GetFiles(fbd.SelectedPath, "*.jp*g", SearchOption.AllDirectories).ToList();
             unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(fbd.SelectedPath, "*.png", SearchOption.AllDirectories)).ToList();
+
+            //TODO gui logic here like disabling these controls and opening the rest.
         }
+        else if (result != DialogResult.Cancel)
+            MessageBox.Show("Unknown error when choosing folder. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    private void chooseDestDirBtn_Click(object sender, EventArgs e)
+    {
+        using var fbd = new FolderBrowserDialog();
+        fbd.UseDescriptionForTitle = true;
+        fbd.Description = "Select Root Folder Where Sorted Items Will Go";
+        DialogResult result = fbd.ShowDialog();
+
+        if (result == DialogResult.OK && !String.IsNullOrWhiteSpace(fbd.SelectedPath) && Directory.Exists(fbd.SelectedPath))
+        {
+            sortedDirRootPath = fbd.SelectedPath;
+        }
+        else if (result != DialogResult.Cancel)
+        {
+            MessageBox.Show("Unknown error when choosing folder. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void applyBtn_Click(object sender, EventArgs e)
+    {
+
     }
 }
