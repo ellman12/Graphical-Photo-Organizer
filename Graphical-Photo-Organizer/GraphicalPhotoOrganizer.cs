936cb12fdf765033d3064370ab39c0d00efa30a5
Elliott <ellduc4@gmail.com>
2022-02-21 12:19:45 -0600

    empty form create

---
+++ Graphical-Photo-Organizer/GraphicalPhotoOrganizer.cs
@@ -0,0 +1,9 @@
+namespace Graphical_Photo_Organizer;
+
+public partial class GraphicalPhotoOrganizer : Form
+{
+    public GraphicalPhotoOrganizer()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Graphical-Photo-Organizer/Program.cs
@@ -0,0 +1,14 @@
+namespace Graphical_Photo_Organizer;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new GraphicalPhotoOrganizer());
+    }    
+}
