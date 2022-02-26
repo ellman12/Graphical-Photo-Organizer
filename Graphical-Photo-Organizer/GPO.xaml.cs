e33f0471b4d38cd8572b9b49fd4dcbc6396089f7
Elliott <ellduc4@gmail.com>
2022-02-26 14:52:17 -0600

    rename GUI

---
+++ Graphical-Photo-Organizer/GPO.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using WinForms = System.Windows.Forms;
+
+namespace Graphical_Photo_Organizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GPO : Window
+    {
+        public GPO()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using var dialog = new WinForms.FolderBrowserDialog();
+            WinForms.DialogResult result = dialog.ShowDialog();
+        }
+    }
+}
