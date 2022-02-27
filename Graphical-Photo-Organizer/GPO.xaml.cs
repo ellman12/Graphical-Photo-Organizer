9dbf04d91abfa919b8a55f930744a42fd4e773a7
Elliott <ellduc4@gmail.com>
2022-02-27 16:55:40 -0600

    can now choose the 2 folders and stuff

--- Graphical-Photo-Organizer/GPO.xaml.cs
+++ Graphical-Photo-Organizer/GPO.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
 using WinForms = System.Windows.Forms;
 // using M = Graphical_Photo_Organizer.Metadata;
 
@@ -15,12 +17,12 @@ namespace Graphical_Photo_Organizer
     {
         //Set during setup
         private List<string> unsortedFiles = new();
-        private string unsortedDirRootPath = "", sortedDirRootPath = "";
-        
+        private string srcDirRootPath = "", destDirRootPath = "";
+
         //Set on file load and stays constant
         private string ext = "";
         // private M.DateTakenSrc dateTakenSrc;
-        
+
         //Set on load and can be changed by user //TODO is this accurate?
         private string ogFilename = "";
         private string destFolderPath = ""; //Folder where the current photo will end
@@ -31,32 +33,84 @@ namespace Graphical_Photo_Organizer
         private int amountSorted = 0;
         private int amountSkipped = 0;
         private int amountDeleted = 0;
-        
+
         public GPO()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Used for getting the source and destination folders. Uses a WinForms folder browser dialog.
+        /// </summary>
+        /// <param name="winTitle">What the folder browser dialog should display</param>
+        /// <returns>The selected folder path.</returns>
+        private static string SelectFolder(string winTitle)
         {
             using var dialog = new WinForms.FolderBrowserDialog();
             dialog.UseDescriptionForTitle = true;
-            dialog.Description = "Select Folder of Images to Sort";
-            WinForms.DialogResult result = dialog.ShowDialog();
+            dialog.Description = winTitle;
+            DialogResult result = dialog.ShowDialog();
 
             if (result == WinForms.DialogResult.OK && !String.IsNullOrWhiteSpace(dialog.SelectedPath))
-            {
-                unsortedFiles = Directory.GetFiles(dialog.SelectedPath, "*.jp*g", SearchOption.AllDirectories).ToList();
-                unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(dialog.SelectedPath, "*.png", SearchOption.AllDirectories)).ToList();
-                unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(dialog.SelectedPath, "*.gif", SearchOption.AllDirectories)).ToList();
-                unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(dialog.SelectedPath, "*.mp4", SearchOption.AllDirectories)).ToList();
-                unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(dialog.SelectedPath, "*.mkv", SearchOption.AllDirectories)).ToList();
+                return dialog.SelectedPath;
+            else
+                return "";
+        }
 
-                unsortedDirRootPath = dialog.SelectedPath;
-                // srcDirLabel.Text = unsortedDirRootPath; //TODO 
+        ///<summary>Checks the folder paths the user chose to make sure they are valid and they can proceed to the sorting.</summary>
+        private void ValidateFolderDirs()
+        {
+            if (unsortedFiles.Count == 0)
+            {
+                MessageBox.Show("There were no supported files found in the source folder provided.", "No supported files found in folder", MessageBoxButton.OK, MessageBoxImage.Error);
+                beginBtn.IsEnabled = false;
+                srcDirRootPath = "";
+                srcDirLabel.Content = "";
             }
-            else if (result != WinForms.DialogResult.Cancel)
-                MessageBox.Show("Unknown error when choosing folder. Please try again.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (srcDirRootPath == destDirRootPath)
+            {
+                MessageBox.Show("You cannot have the same source and destination folder path!", "Same folder path specified", MessageBoxButton.OK, MessageBoxImage.Error);
+                beginBtn.IsEnabled = false;
+                srcDirRootPath = "";
+                srcDirLabel.Content = "";
+                destDirRootPath = "";
+                destDirLabel.Content = "";
+            }
+            else if (!String.IsNullOrWhiteSpace(srcDirRootPath) && !String.IsNullOrWhiteSpace(destDirRootPath))
+            {
+                beginBtn.IsEnabled = true;
+            }
+            else
+            {
+                currentItemGroupBox.IsEnabled = false;
+                beginBtn.IsEnabled = false;
+            }
+        }
+
+        private void chooseSrcBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string path = SelectFolder("Select Folder of Images to Sort");
+
+            //unsortedFiles.Clear(); //Clear if user changed to different src folder
+            unsortedFiles = Directory.GetFiles(path, "*.jp*g", SearchOption.AllDirectories).ToList();
+            unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(path, "*.png", SearchOption.AllDirectories)).ToList();
+            unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(path, "*.gif", SearchOption.AllDirectories)).ToList();
+            unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(path, "*.mp4", SearchOption.AllDirectories)).ToList();
+            unsortedFiles = unsortedFiles.Concat(Directory.GetFiles(path, "*.mkv", SearchOption.AllDirectories)).ToList();
+
+            srcDirRootPath = path;
+            srcDirLabel.Content = srcDirRootPath;
+            ValidateFolderDirs();   
+        }
+
+        private void chooseDestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string path = SelectFolder("Select Root Folder Where Sorted Items Will Go");
+
+            destDirRootPath = path;
+            destDirLabel.Content = destDirRootPath;
+
+            ValidateFolderDirs();
         }
     }
 }
