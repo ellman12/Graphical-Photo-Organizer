643bdd2ab399c4565b10486f8a3f8b5575dc614d
Elliott <ellduc4@gmail.com>
2022-03-13 16:18:08 -0500

    implement checkbox to toggle MsgBox warning when deleting current item

--- Graphical-Photo-Organizer/GPO.xaml.cs
+++ Graphical-Photo-Organizer/GPO.xaml.cs
@@ -320,8 +320,16 @@ public partial class GPO
 
     private async void DeleteBtn_Click(object sender, RoutedEventArgs e)
     {
-        MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this item?", "Delete this item?", MessageBoxButton.YesNo, MessageBoxImage.Question);
-        if (result == MessageBoxResult.Yes)
+        if (delWarnCheckBox.IsChecked == false)
+            await Recycle();
+        else if (delWarnCheckBox.IsChecked == true)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this item?", "Delete this item?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+                await Recycle();
+        }
+
+        async Task Recycle()
         {
             string deletePath = unsortedFiles[0];
             unsortedFiles.RemoveAt(0);
