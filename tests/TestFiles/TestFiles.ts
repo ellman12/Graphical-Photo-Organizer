dccfcbf3e582b1178053482a65f493eca583484c
Elliott DuCharme <ellduc4@gmail.com>
2025-11-04 12:02:39 -0600

    Add test files and data for them

---
+++ tests/TestFiles/TestFiles.ts
@@ -0,0 +1,128 @@
+export type TestFile = {
+    filename: string;
+    metadataDateTaken: string | null;
+    filenameDateTaken: string | null;
+};
+
+export const TestFiles: TestFile[] = [
+    {
+        filename: "2021122301175100_s.jpg",
+        metadataDateTaken: "2021-12-23T01:17:51.000Z",
+        filenameDateTaken: "2021-12-23T01:17:51.000Z",
+    },
+    {
+        filename: "20220723_090814.jpg",
+        metadataDateTaken: "2022-07-23T09:08:14.000Z",
+        filenameDateTaken: "2022-07-23T09:08:14.000Z",
+    },
+    {
+        filename: "c142964.png",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "snuggles.jpeg",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "Hello World.png",
+        metadataDateTaken: "2021-01-12T14:47:00.000Z",
+        filenameDateTaken: null,
+    },
+    {
+        filename: "hi-cat.gif",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "IMG_0125.JPG",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "IMG_1148.JPG",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "IMG_20230511_102100.jpg",
+        metadataDateTaken: "2023-05-11T10:21:00.000Z",
+        filenameDateTaken: "2023-05-11T10:21:00.000Z",
+    },
+    {
+        filename: "IMG_20230527_225731.jpg",
+        metadataDateTaken: "2023-05-27T22:57:31.000Z",
+        filenameDateTaken: "2023-05-27T22:57:31.000Z",
+    },
+    {
+        filename: "IMG_20230528_112147.jpg",
+        metadataDateTaken: "2023-05-28T11:21:47.000Z",
+        filenameDateTaken: "2023-05-28T11:21:47.000Z",
+    },
+    {
+        filename: "IMG_20230702_105928.jpg",
+        metadataDateTaken: "2023-07-02T11:59:29.000Z",
+        filenameDateTaken: "2023-07-02T10:59:28.000Z",
+    },
+    {
+        filename: "IMG_5021.MOV",
+        metadataDateTaken: "2023-06-06T21:15:20.000Z",
+        filenameDateTaken: null,
+    },
+    {
+        filename: "IMG_5109.JPG",
+        metadataDateTaken: "2023-06-07T09:04:47.000Z",
+        filenameDateTaken: null,
+    },
+    {
+        filename: "k6mq4lmuype71.jpg",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "n1bsrky1y2h71.jpg",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "Screenshot_20200803-170750_Snapchat.jpg",
+        metadataDateTaken: "2020-08-03T17:07:50.000Z",
+        filenameDateTaken: "2020-08-03T17:07:50.000Z",
+    },
+    {
+        filename: "Screenshot_20220414-142641_Google.jpg",
+        metadataDateTaken: "2022-04-14T14:26:41.000Z",
+        filenameDateTaken: "2022-04-14T14:26:41.000Z",
+    },
+    {
+        filename: "Screenshot_20230619-183636.jpg",
+        metadataDateTaken: null,
+        filenameDateTaken: "2023-06-19T18:36:36.000Z",
+    },
+    {
+        filename: "Snapchat-619882372.jpg",
+        metadataDateTaken: "2023-03-24T13:29:01.000Z",
+        filenameDateTaken: null,
+    },
+    {
+        filename: "tempFileForShare_20220513-215422.jpg",
+        metadataDateTaken: null,
+        filenameDateTaken: "2022-05-13T21:54:22.000Z",
+    },
+    {
+        filename: "vibing cat.gif",
+        metadataDateTaken: null,
+        filenameDateTaken: null,
+    },
+    {
+        filename: "VID_20230622_110234.mp4",
+        metadataDateTaken: "2023-06-22T16:02:34.000Z",
+        filenameDateTaken: "2023-06-22T11:02:34.000Z",
+    },
+    {
+        filename: "VID_20230706_102721.mp4",
+        metadataDateTaken: "2023-07-06T17:27:21.000Z",
+        filenameDateTaken: "2023-07-06T10:27:21.000Z",
+    },
+];
